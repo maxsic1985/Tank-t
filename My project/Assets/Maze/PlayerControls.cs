5ca3d52572c16c3ee09673fe223b8a4aff598308
Maksim.S <maxsic85@mail.ru>
2022-07-07 16:47:35 +0300

    UniRx PlayerControls

--- My project/Assets/Maze/PlayerControls.cs
+++ My project/Assets/Maze/PlayerControls.cs
@@ -1,22 +1,23 @@
-﻿using UnityEngine;
+﻿using UniRx;
+using UnityEngine;
+using Zenject;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControls : MonoBehaviour, IPlayer
 {
-    public float Speed = 2;
-
+    public float Speed = 1;
     private Rigidbody2D componentRigidbody;
 
-    private void Start()
+    [Inject]
+    private void Init()
     {
         componentRigidbody = GetComponent<Rigidbody2D>();
+        var resetVelocity= Observable.EveryFixedUpdate().Subscribe(_ => { componentRigidbody.velocity = Vector2.zero; });
+        var inpusKeyKodeStream = Observable.EveryFixedUpdate().Where(_ => Input.anyKey);
+        var leftClick = inpusKeyKodeStream.Where(_ => Input.GetKey(KeyCode.LeftArrow)).Subscribe(_ => { componentRigidbody.velocity += Vector2.left * Speed; });
+        var rightClick = inpusKeyKodeStream.Where(_ => Input.GetKey(KeyCode.RightArrow)).Subscribe(_ => { componentRigidbody.velocity += Vector2.right * Speed; });
+        var downClick = inpusKeyKodeStream.Where(_ => Input.GetKey(KeyCode.DownArrow)).Subscribe(_ => { componentRigidbody.velocity += Vector2.down * Speed; });
+        var upClick = inpusKeyKodeStream.Where(_ => Input.GetKey(KeyCode.UpArrow)).Subscribe(_ => { componentRigidbody.velocity += Vector2.up * Speed; });
     }
 
-    private void Update()
-    {
-        componentRigidbody.velocity = Vector2.zero;
-        if (Input.GetKey(KeyCode.LeftArrow)) componentRigidbody.velocity += Vector2.left * Speed;
-        if (Input.GetKey(KeyCode.RightArrow)) componentRigidbody.velocity += Vector2.right * Speed;
-        if (Input.GetKey(KeyCode.UpArrow)) componentRigidbody.velocity += Vector2.up * Speed;
-        if (Input.GetKey(KeyCode.DownArrow)) componentRigidbody.velocity += Vector2.down * Speed;
-    }
 }
