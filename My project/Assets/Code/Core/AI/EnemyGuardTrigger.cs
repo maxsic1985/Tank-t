c4e59cd6facb588a0b9bfcbb8c84fe9d153dfb30
Maksim.S <maxsic85@mail.ru>
2022-07-07 12:04:49 +0300

    Guard Zone

--- My project/Assets/Code/Core/AI/EnemyGuardTrigger.cs
+++ My project/Assets/Code/Core/AI/EnemyGuardTrigger.cs
@@ -7,15 +7,17 @@ public class EnemyGuardTrigger : MonoBehaviour
     public Action<GameObject> TriggerEnter;
     public Action<GameObject> TriggerExit;
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerEnter(Collider other)
     {
-      if(other?.GetComponentInParent<PlayerControls>())   TriggerEnter?.Invoke(other.gameObject);
+        if (other?.GetComponentInParent<PlayerControls>()) TriggerEnter?.Invoke(other.gameObject);
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+
+    private void OnTriggerExit(Collider other)
     {
         if (other?.GetComponentInParent<PlayerControls>()) TriggerExit?.Invoke(other.gameObject);
     }
-}
 
+}
 
